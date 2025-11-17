79e07090b759316fe1a92d53a52fc30f382b3613
gabrielpoll <gabrieldarosa.poll@gmail.com>
2025-11-17 18:51:20 -0300

    create the player factory function

--- script.js
+++ script.js
@@ -18,3 +18,35 @@ let Gameboard = () => {
   };
   return { board };
 };
+
+let Player = (name, symbol) => {
+  let playerName = name;
+  let playerSymbol = symbol;
+  let playerTurn = false;
+  let wins = 0;
+
+  const getName = () => playerName;
+
+  const getSymbol = () => playerSymbol;
+
+  const isTurn = () => playerTurn;
+
+  const setTurn = (bool) => {
+    playerTurn = bool;
+  };
+
+  const addWin = () => {
+    wins++;
+  };
+
+  const getWins = () => wins;
+
+  return {
+    getName,
+    getSymbol,
+    isTurn,
+    setTurn,
+    addWin,
+    getWins
+  };
+};
